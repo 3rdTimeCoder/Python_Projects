e94c7fd22bef0fd6faf45c75f76c1bd5e979ffff
Nomahlubi Hadebe <nosifan022@student.wethinkcode.co.za>
2023-08-01 12:36:50 +0200

    audio_helper.py to help with text to speech

---
+++ assistant/audio_helper.py
@@ -0,0 +1,26 @@
+from gtts import gTTS
+import pygame
+
+
+def text_to_mp3(text):
+    print("Starting text to speech conversion...")
+    audio = gTTS(text=text, lang="en", slow=False)
+    audio.save("text.mp3")
+    print("Completed text to speech conversion...")
+
+
+def play_audio(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+
+
+if __name__ == "__main__":
+    file = "story"
+    story = open(file + ".txt")
+    text_to_mp3(story.read())
+    story.close()
+    play_audio("text.mp3")
+
